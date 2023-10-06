833811d0183abdb1d8501c8c3c59c178cc504322
Frank Osadolor <2osadf62@solent.ac.uk>
2023-10-06 12:46:56 +0100

    2nd TaskS

--- week2/if_statements.py
+++ week2/if_statements.py
@@ -1,7 +1,17 @@
 # i don't know what i am doing haha!..
 
+print ("Please enter the activity to be performed.")
+ activity = input()
+
+if activity == "calculate":
+    print("performing calculations...")
+
+
+
+
 print ("hello there! how are you doing? i'm sure you love books, What type of book is this?")
 type_of_book = input()
 
 if type_of_book == "adventure":
     print(f"I like {type_of_book} books!")git
+
