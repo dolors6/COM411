dcfbade0315081f202eaa464f1be3a3c1b6bc7e3
Frank Osadolor <2osadf62@solent.ac.uk>
2023-10-13 12:24:37 +0100

    Added code to demonstrate the use of a while loop with ASCII art.

--- week 3/while_loops.py
+++ week 3/while_loops.py
@@ -6,6 +6,8 @@ while count <= apple:
     print("Removed apple.")
     count += 1
 
+#first task
+
 print("How many obstacles must i avoid?")
 obstacle = int(input())
 count = 1
@@ -13,4 +15,18 @@ while count <= obstacle:
     print (f"Avoiding... Done! {count} obstacles avoided.")
     count += 1
 
+#second task
+
 print("All obstacles have been avoided.")
+
+#third task
+
+print("How many bars should be charged?")
+bars = int(input())
+count = 1
+
+while count <= bars:
+    print(f"Charging: {'█ ' * count}")
+    count += 1
+
+print("The battery is fully charged.")
