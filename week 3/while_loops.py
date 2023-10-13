90465c5b9c2ac147a4a3b2cf5312a236f17d2981
Frank Osadolor <2osadf62@solent.ac.uk>
2023-10-13 12:07:44 +0100

    tasks_for_week3

--- week 3/while_loops.py
+++ week 3/while_loops.py
@@ -3,5 +3,14 @@ print("How many apples should i remove?")
 apple = int(input())
 count = 1
 while count <= apple:
-    print ("Removed apple.")
+    print("Removed apple.")
     count += 1
+
+print("How many obstacles must i avoid?")
+obstacle = int(input())
+count = 1
+while count <= obstacle:
+    print (f"Avoiding... Done! {count} obstacles avoided.")
+    count += 1
+
+print("All obstacles have been avoided.")
