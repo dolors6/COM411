22897da37e158a4c8c805127cdf489bfc32f938a
frank <2osadf62@solent.ac.uk>
2023-10-20 12:51:19 +0100

    practise 2.2

--- week 4 (functions)/built_in_practise.py
+++ week 4 (functions)/built_in_practise.py
@@ -6,3 +6,18 @@ conv = ord(usr)
 print(f"The ASCII code for the {usr} is {conv}")
 print("Program Ended!")
 
+
+#now moving unto task 2
+
+print("Program Started!")
+print("Please enter an ASCII code:")
+
+urm = abs(int(input()))
+if urm in range(32, 127):
+    letter = chr(urm)
+
+    print(f"The character represented by the ASCII code {urm} is {letter}")
+else:
+    print("re-enter your input:")
+
+print("Program Ended!")
