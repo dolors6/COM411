6e8112731332c5ae79aad146a203ff31fccc6df4
frank <2osadf62@solent.ac.uk>
2023-12-01 12:37:56 +0000

    Ip exercises

--- prx fortune frank/1st.py
+++ prx fortune frank/1st.py
@@ -1,3 +1,16 @@
-print("Enter age :")
-age = int(input())
-if age == 33
+# ask user for age.
+# This time use (input) to print and collect data.
+# name input variable
+
+age_user = input("Enter your age : ")
+age_user = int(age_user)
+
+# Calculate the birth year
+
+year_of_birth = 2023 - age_user
+
+# if age_user
+print(f"You were born in {year_of_birth}")
+# else:
+#    print("Re-enter input, Enter a valid number.")
+#i need to use while loop to re-display the question, until the valid answer is given..
