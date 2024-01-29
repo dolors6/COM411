aafe660553bdd7e57d5fc2747ef6c7503a0dcd03
frank <2osadf62@solent.ac.uk>
2024-01-29 18:51:02 +0000

    Testing PyCharm with GitHub.

--- Xmas Re-tasks/WEEK1/ifstat.py
+++ Xmas Re-tasks/WEEK1/ifstat.py
@@ -2,7 +2,9 @@ ride = input("Do you want to go on rides? y/n")
 
 if ride == "y":
     age = int(input("For you to be able to go on this ride you must be over 18, How old are you?"))
+
 # age >= 18 and age < 60:
+
     if 60 > age >= 18:
         print(f"You are good to enter the ride after all you are {age}")
     elif age == 61:
