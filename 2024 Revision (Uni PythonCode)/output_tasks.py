d93dae2891d42c6cd6b860b727ce5c98f9057e3d
frank <2osadf62@solent.ac.uk>
2024-01-29 19:12:27 +0000

    Updated code to display a longer multi-line message.

--- 2024 Revision (Uni PythonCode)/output_tasks.py
+++ 2024 Revision (Uni PythonCode)/output_tasks.py
@@ -1,3 +1,8 @@
 # this is a comment
 print("Welcome to COM411!")
 print("In week 1 we will learn...")
+print("""
+      How to use Git and GitHub
+      ...How to output to the screen
+      ...How to get user input
+I hope you are enjoying the lesson thus far!""")
