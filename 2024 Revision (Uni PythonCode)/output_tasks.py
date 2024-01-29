d21d455f6b4cafc81e9ba9cc3a22618d4903ad97
frank <2osadf62@solent.ac.uk>
2024-01-29 19:33:37 +0000

    Added code to display an ASCII art robot.

--- 2024 Revision (Uni PythonCode)/output_tasks.py
+++ 2024 Revision (Uni PythonCode)/output_tasks.py
@@ -16,3 +16,8 @@ print("\' Displays a single quote")
 # if you look at the code above you will see how to use the backslash in your text, it enables you to do tasks which seem impossible within a print statement
 
 print("\t I am Programming!")
+
+print("##########")
+print("#        #")
+print("#        #")
+print("##########")
