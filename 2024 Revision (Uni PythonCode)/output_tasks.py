936acdb5f39682cd82874896c013568e2350936b
frank <2osadf62@solent.ac.uk>
2024-01-29 19:28:48 +0000

    Added code to display escape characters.

--- 2024 Revision (Uni PythonCode)/output_tasks.py
+++ 2024 Revision (Uni PythonCode)/output_tasks.py
@@ -6,3 +6,11 @@ print("""
       ...How to output to the screen
       ...How to get user input
 I hope you are enjoying the lesson thus far!""")
+
+# to be more in control of the display of your text make sure you know how to use "\" this is called the backslash
+print("\n Displays a new line")
+print("\t Displays a tab space")
+print("\\ Displays a back slash")
+print("\" Displays a double quote")
+print("\' Displays a single quote")
+# if you look at the code above you will see how to use the backslash in your text, it enables you to do tasks which seem impossible within a print statement
