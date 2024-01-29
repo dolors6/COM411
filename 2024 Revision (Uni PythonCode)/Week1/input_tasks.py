46cb4bbee3860b0e80e7fde95eaf8141e7682f4b
frank <2osadf62@solent.ac.uk>
2024-01-29 19:38:48 +0000

    Added code to read user input.

---
+++ 2024 Revision (Uni PythonCode)/Week1/input_tasks.py
@@ -0,0 +1,6 @@
+print("What is your name?")
+name = input()
+print(f"It is nice to meet you {name}")
+# ----------------------
+user_name = input("What is your name?")
+print(f"It is nice to meet you {user_name}")
